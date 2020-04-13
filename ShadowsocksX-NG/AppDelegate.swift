b541176cd32f27541808fc8fbcdc85caab375df1
wzdnzd <glenn@oiizz.com>
2020-04-13 23:56:56 +0800

    简化代码

--- ShadowsocksX-NG/AppDelegate.swift
+++ ShadowsocksX-NG/AppDelegate.swift
@@ -385,18 +385,16 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         if let profile = ServerProfileManager.instance.getActiveProfile() {
             if profile.isValid() {
                 // Show window
-                DispatchQueue.global().async {
+                DispatchQueue.main.async {
                     if self.qrcodeWinCtrl != nil{
                         self.qrcodeWinCtrl.close()
                     }
                     self.qrcodeWinCtrl = SWBQRCodeWindowController(windowNibName: "SWBQRCodeWindowController")
                     self.qrcodeWinCtrl.qrCode = profile.URL()!.absoluteString
                     self.qrcodeWinCtrl.title = profile.title()
-                    DispatchQueue.main.async {
-                        self.qrcodeWinCtrl.showWindow(self)
-                        NSApp.activate(ignoringOtherApps: true)
-                        self.qrcodeWinCtrl.window?.makeKeyAndOrderFront(nil)
-                    }
+                    self.qrcodeWinCtrl.showWindow(self)
+                    NSApp.activate(ignoringOtherApps: true)
+                    self.qrcodeWinCtrl.window?.makeKeyAndOrderFront(nil)
                 }
                 return
             } else {
@@ -409,8 +407,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         userNote.title = errMsg
         userNote.soundName = NSUserNotificationDefaultSoundName
         
-        NSUserNotificationCenter.default
-            .deliver(userNote);
+        NSUserNotificationCenter.default.deliver(userNote);
     }
     
     @IBAction func scanQRCodeFromScreen(_ sender: NSMenuItem) {
